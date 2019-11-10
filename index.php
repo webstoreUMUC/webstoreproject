7df9684253e0ae8d2f76765a61d18fd0378dc78d
Eric Hayes <ehayes15@student.umuc.edu>
2019-11-10 12:20:42 -0500

    Add files via upload
    "Added navabars, links and dropdowns"

--- index.php
+++ index.php
@@ -1,48 +1,107 @@
-<?php
-
-
-require_once('../database.php');
-
-?>
-
 <!doctype html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Food Bank</title>
-    <link rel="stylesheet" type="text/css" href="../styles/main.css">
-    <!--No Css Applied Yet -->
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Gen 3 Market</title>
+    <!--<link rel="stylesheet" href="styles/main.css">-->
 </head>
+
 <body>
-<header><h1>FoodBank - Place An Order</h1></header>
-<div id="logo">
-    <img id="fb" src="../images/foodbank.png">
-</div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="images/foodbank.png" width="30" height="30" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<main>
-    <h1>Customer Data</h1>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Cloths
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Men's</a>
+                        <a class="dropdown-item" href="#">Women's</a>
+                        <a class="dropdown-item" href="#">Kids's</a>
+                    </div>
+                </li>
 
-    <form action="customer.php" method="post">
-        <table>
-            <tr>
-                <td>First Name:</td>
-                <td><input type="text" name="firstName"></td>
-            </tr>
-            <tr>
-                <td>Surname:</td>
-                <td><input type="text" name="lastName"></td>
-            </tr>
-            <tr>
-                <td>Phone Number:</td>
-                <td><input type="tel" name="phoneNumber"></td>
-            </tr>
-        </table>
-        <input type="submit" value="Next">
-    </form>
-    <br>
-</main>
-<footer>
-    <br><p>&copy; <?php echo date("Y"); ?> FoodBank</p>
-</footer>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Food
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Beverages</a>
+                        <a class="dropdown-item" href="#">Canned Goods</a>
+                        <a class="dropdown-item" href="#">Vegetables</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Electronics
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Phones</a>
+                        <a class="dropdown-item" href="#">Laptops</a>
+                        <a class="dropdown-item" href="#">Accessories</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Specials
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Open Box</a>
+                        <a class="dropdown-item" href="#">Discontinued</a>
+                    </div>
+                </li>                
+            </ul>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Account
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/foodBankCustomer/index.php">Login/Register</a>
+                        <a class="dropdown-item" href="/foodBankAdmin">Admin</a>
+                    </div>
+                </li>
+                <l1 class="nav-item">
+                    <a class="nav-link" href="#">Contact <span class="sr-only"></span></a>
+                </l1>
+                <l1 class="nav-item">
+                    <a class="nav-link" href="#">About Us<span class="sr-only"></span></a>
+                </l1>
+            </ul>
+        </div>
+    </nav>
+    
+
+    <div class="panel panel-default">
+        <div class="panel-footer">
+            <br>
+            <p>&copy; <?php echo date("Y"); ?> Gen 3 Market</p>
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
