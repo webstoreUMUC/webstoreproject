ad51bc8d7ef0859777d0829600f5c9269b882d5d
Eric Hayes <ehayes15@student.umuc.edu>
2019-11-10 12:18:54 -0500

    Add files via upload
    Added navbars, links and dropdowns to the main index page.

--- index.php
+++ index.php
@@ -1,30 +1,48 @@
+<?php
+
+
+require_once('../database.php');
+
+?>
 
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/main.css">
-    <title>Gen3Market</title>
+    <title>Food Bank</title>
+    <link rel="stylesheet" type="text/css" href="../styles/main.css">
+    <!--No Css Applied Yet -->
 </head>
 <body>
+<header><h1>FoodBank - Place An Order</h1></header>
+<div id="logo">
+    <img id="fb" src="../images/foodbank.png">
+</div>
+
 <main>
-    <header><h1>Gen3Market</h1></header>
-    <div id="logo">
-        <img id="fb" src="images/foodbank.png">
-    </div>
-    <h1>Menu</h1>
-    <ul>
-        <li>
-            <a href="gen3marketAdmin">Gen3market Admin</a>
-        </li>
-        <li>
-            <a href="gen3marketCustomer">Gen3market Customer</a>
-        </li>
-    </ul>
+    <h1>Customer Data</h1>
+
+    <form action="customer.php" method="post">
+        <table>
+            <tr>
+                <td>First Name:</td>
+                <td><input type="text" name="firstName"></td>
+            </tr>
+            <tr>
+                <td>Surname:</td>
+                <td><input type="text" name="lastName"></td>
+            </tr>
+            <tr>
+                <td>Phone Number:</td>
+                <td><input type="tel" name="phoneNumber"></td>
+            </tr>
+        </table>
+        <input type="submit" value="Next">
+    </form>
+    <br>
 </main>
 <footer>
-    <br><p>&copy; <?php echo date("Y"); ?> Gen3market</p>
+    <br><p>&copy; <?php echo date("Y"); ?> FoodBank</p>
 </footer>
+
 </body>
 </html>
